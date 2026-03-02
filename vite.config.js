08491829ed3fee2469c24e75c95ed8f64907b12b
YOUR_NAME <you@example.com>
2026-03-02 21:16:08 +0530

    Update portfolio code

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base:'/my-portfolio'
+  plugins: [react(), tailwindcss()],
+  base: '/my-portfolio/'  // important for GitHub Pages
 })
